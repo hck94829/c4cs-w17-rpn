3672a5a3913a377dd3cc8217e73b773e34ed72b3
HuChengkai <hucheng@umich.edu>
2017-03-10 14:31:36 -0500

    work

--- rpn.py
+++ rpn.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-    pass
+    stack = []
+    for operand in arg.split():
+        if operand == '+':
+            stack.append(stack.pop() + stack.pop())
+        else:
+            stack.append(float(operand))
+    return stack.pop()
 
 def main():
     while True:
-        calculate(input("rpn calc> "))
+        rst = calculate(input("rpn calc> "))
+        print('Result:',rst)
 
 if __name__ == '__main__':
     main()
